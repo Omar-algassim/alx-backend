a47e99edd370199a94f09755476f329a684e1583
Omar-Algassim <omarggg2@gamil.com>
2024-07-20 15:52:53 +0300

    Revert "start from 0"
    This reverts commit 3c39692d5b8e87ca6a24f17ec2c3dbf6cb2c10cf.

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,4 +4,4 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """return the page range crosponding on pag number and size"""
-    return ((((page - 1) * page_size) - page_size, (page - 1) * page_size))
+    return (((page * page_size) - page_size, page * page_size))
