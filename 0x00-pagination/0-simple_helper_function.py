ffdf31a864b35cb04d1bf6708af8d40b59c5421b
Omar-Algassim <omarggg2@gamil.com>
2024-07-21 15:45:08 +0300

    change the annonation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """page range"""
+from typing import Tuple
 
-
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple:
     """return the page range crosponding on pag number and size"""
     return (((page * page_size) - page_size, page * page_size))
