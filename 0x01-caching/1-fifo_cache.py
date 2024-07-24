1121627323756f7761fb8a677ee729ac13e8f24e
Omar-Algassim <omarggg2@gamil.com>
2024-07-24 23:21:31 +0300

    pycodestyle fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,8 +14,8 @@ class FIFOCache(BaseCaching):
         if not key or not item:
             return
         key_list = [*self.cache_data.keys()]
-        if len(self.cache_data) >= self.MAX_ITEMS \
-            and key not in self.cache_data:
+        if len(self.cache_data) >= self.MAX_ITEMS\
+                and key not in self.cache_data:
             self.cache_data.pop(key_list[0])
             print(f"DISCARD: {key_list[0]}")
         self.cache_data[key] = item
