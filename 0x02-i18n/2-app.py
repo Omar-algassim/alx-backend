da27feac7554625ee5f8f890ae40fc0f7987898c
Omar-Algassim <omarggg2@gamil.com>
2024-07-30 16:20:49 +0300

    request best lanugue

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Flask app"""
+from flask import render_template, Flask
+from flask_babel import Babel, request
+
+
+class Config():
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get best match languge"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def basic():
+    """basic rote 'hello world'"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
